9a5db22bc2b98b5401f871a259a817dac18b2989
Mike Jones <mike@westerncapelabs.com>
2014-10-13 16:28:52 +0200

    Bump version to 0.1.0a post release

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="django-snappy-vumi-bouncer",
-    version="0.1.0",
+    version="0.1.0a",
     url='https://github.com/westerncapelabs/django-snappy-vumi-bouncer',
     license='BSD',
     description=(
